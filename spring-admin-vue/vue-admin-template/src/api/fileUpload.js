b06446147b7dcdda83e0cfe82edb83111a5ede7c
316638644@qq.com <316638644@qq.com>
2020-08-01 21:34:17 +0800

    all

--- spring-admin-vue/vue-admin-template/src/api/fileUpload.js
+++
@@ -1,6 +0,0 @@
-import request from '@/utils/request'
-
-// 删除图片
-export function deleteImage(params) {
-  return request({ url: '/upload/delete', method: 'delete', data: { url: params }})
-}
--- spring-admin-vue/vue-admin-template/src/utils/get-page-title.js
+++
@@ -1,8 +0,0 @@
-import defaultSettings from '@/settings'
-
-export default function getPageTitle(pageTitle) {
-  if (pageTitle) {
-    return `${pageTitle} - ${defaultSettings.title}`
-  }
-  return `${defaultSettings.title}`
-}
